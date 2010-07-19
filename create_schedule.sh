a75a62dce69467a968f4d47edd9ca29277b18772
Prasanna Srinivasan <to.srini@gmail.com>
2010-07-19 15:42:32 +0530

    Adding other config files and third-party jars.

---
+++ create_schedule.sh
@@ -0,0 +1,2 @@
+java -cp ./lib/schedule_converter.jar:./lib/jxl.jar:./lib/dom4j-1.6.1.jar:./lib/geronimo-stax-api_1.0_spec-1.0.jar:./lib/ooxml-schemas-1.0.jar:./lib/xmlbeans-2.3.0.jar:./lib/poi-ooxml-3.5-beta6-20090622.jar:./lib/poi-3.5-beta6-20090622.jar:./ org.isha.tco.schedule.ScheduleConverter2007 $*
+
